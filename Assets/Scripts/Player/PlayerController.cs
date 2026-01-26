d89e522b17c8ae89f9c6b3dc1cbe3c18ebb9a991
fabroche <kraxusmmo@gmail.com>
2026-01-26 22:20:59 +0000

    feat: Jump and grounding detecting implemented

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,22 +5,48 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float moveSpeed = 5.0f;
 
-    private Rigidbody2D rb;
-    private float moveInput;
-    
+    private Rigidbody2D _rb;
+    private float _moveInput;
+
+    [Header("Jump Settings")] public float jumpForce = 10f;
+
+    [Header("Ground Check")] public Transform groundCheckPoint;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private bool _isGrounded;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
+        _isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        _moveInput = Input.GetAxisRaw("Horizontal");
+        Jump();
     }
 
     void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        _rb.linearVelocity = new Vector2(_moveInput * moveSpeed, _rb.linearVelocity.y);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheckPoint == null) return;
+
+        Gizmos.color = _isGrounded ? Color.green : Color.red;
+        Gizmos.DrawWireSphere(groundCheckPoint.position, groundCheckRadius);
     }
 }
