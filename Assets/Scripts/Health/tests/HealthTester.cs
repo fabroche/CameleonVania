0c17552048959cab200f5ff88bf6d42b1ed9ed1d
fabroche <kraxusmmo@gmail.com>
2026-01-27 20:47:40 +0000

    feat: health bar mechanich implemented, no UI

---
+++ Assets/Scripts/Health/tests/HealthTester.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+public class HealthTester : MonoBehaviour
+{
+    [SerializeField] private Health health;
+
+    private void Start()
+    {
+        if (health == null)
+        {
+            health = GetComponent<Health>();
+        }
+
+        health.OnHealthChanged += OnHealthChanged;
+        health.OnDeath += OnDeath;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            health.TakeDamage(20f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            health.Heal(15f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            health.TakeDamage(1000f);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (health != null)
+        {
+            health.OnHealthChanged -= OnHealthChanged;
+            health.OnDeath -= OnDeath;
+        }
+    }
+
+    private void OnHealthChanged(float newHealth)
+    {
+        Debug.Log($"[EVENT] Health changed: {newHealth}");
+    }
+
+    private void OnDeath()
+    {
+        Debug.Log($"[EVENT] {gameObject.name} died!");
+    }
+}
